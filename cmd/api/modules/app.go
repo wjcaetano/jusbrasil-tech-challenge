49b9ad51fce3de57a8de5e66d2f42b0bbf019d36
Willian Caetano <caetano.willian@gmail.com>
2024-11-21 11:06:13 -0300

    Make adjustment to run the project

---
+++ cmd/api/modules/app.go
@@ -0,0 +1,21 @@
+package modules
+
+import (
+	"jusbrasil-tech-challenge/internal/config"
+
+	"go.uber.org/fx"
+)
+
+func NewApp() *fx.App {
+	options := []fx.Option{
+		InternalModule,
+	}
+
+	if !config.IsLocalScope() {
+		options = append(options, fx.NopLogger)
+	}
+
+	return fx.New(
+		fx.Options(options...),
+	)
+}
