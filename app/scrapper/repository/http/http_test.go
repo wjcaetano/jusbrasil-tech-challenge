58afc6a049df44cde7d91c5bb9c804247f073fe1
Willian Caetano <caetano.willian@gmail.com>
2024-11-27 00:27:36 -0300

    Rename fields and mapping json tags to entities

--- app/scrapper/repository/http/http_test.go
+++ app/scrapper/repository/http/http_test.go
@@ -30,8 +30,8 @@ const (
 		`
 )
 
-func TestNewScraperRepository(t *testing.T) {
-	repo := NewScraperRepository()
+func TestNewScrapperRepository(t *testing.T) {
+	repo := NewScrapperRepository()
 	t.Run("should return not nil a scraper repository", func(t *testing.T) {
 		assert.NotNil(t, repo)
 	})
@@ -41,7 +41,7 @@ func TestNewScraperRepository(t *testing.T) {
 	})
 }
 
-func TestScraperRepository_FetchPage(t *testing.T) {
+func TestScrapperRepository_FetchPage(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte(tableHTML))
@@ -58,7 +58,7 @@ func TestScraperRepository_FetchPage(t *testing.T) {
 								</tr>
 							`
 
-		scraper := NewScraperRepository()
+		scraper := NewScrapperRepository()
 
 		content, err := scraper.FetchPage(server.URL)
 
@@ -70,7 +70,7 @@ func TestScraperRepository_FetchPage(t *testing.T) {
 		invalidURL := "http://invalid-url"
 		expectedError := "failed to fetch page: Get \"http://invalid-url\": dial tcp: lookup invalid-url on 127.0.0.53:53: server misbehaving"
 
-		scraper := NewScraperRepository()
+		scraper := NewScrapperRepository()
 		content, err := scraper.FetchPage(invalidURL)
 
 		require.Error(t, err)
