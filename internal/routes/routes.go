bd10f50b555445818e868591aebd35ac07990a4a
Willian Caetano <caetano.willian@gmail.com>
2024-11-27 00:26:54 -0300

    Adjust routes and router to receive routes from scrapper

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,15 +1,18 @@
 package routes
 
 import (
+	"jusbrasil-tech-challenge/app/scrapper/entrypoint/rest/scrapper"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func RegisterRoutes(router *chi.Mux) {
+func RegisterRoutes(router *chi.Mux, scrapperHandler *scrapper.Handler) {
 	router.Get("/", homeHandler)
 	router.Get("/health", healthHandler)
+
+	scrapperHandler.RegisterScrapperRoutes(router)
 }
 
 func homeHandler(w http.ResponseWriter, _ *http.Request) {
