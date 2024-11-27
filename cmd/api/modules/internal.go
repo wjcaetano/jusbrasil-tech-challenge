a8513ea37e610eceac2890c03f90e3795ee2097f
Willian Caetano <caetano.willian@gmail.com>
2024-11-27 00:28:04 -0300

    Implement dependencies injection

--- cmd/api/modules/internal.go
+++ cmd/api/modules/internal.go
@@ -1,10 +1,14 @@
 package modules
 
 import (
+	"jusbrasil-tech-challenge/app/scrapper/entrypoint/rest/scrapper"
 	"jusbrasil-tech-challenge/internal/config"
 	"jusbrasil-tech-challenge/internal/db"
 	"jusbrasil-tech-challenge/internal/router"
+	"jusbrasil-tech-challenge/internal/routes"
 	"jusbrasil-tech-challenge/internal/server"
+
+	"github.com/go-chi/chi/v5"
 
 	"go.uber.org/fx"
 )
@@ -17,5 +21,10 @@ var internalFactory = fx.Provide(
 
 var InternalModule = fx.Options(
 	internalFactory,
-	fx.Invoke(server.StartHTTPServer),
+	fx.Invoke(
+		server.StartHTTPServer,
+		func(router *chi.Mux, scrapperHandler *scrapper.Handler) {
+			routes.RegisterRoutes(router, scrapperHandler)
+		},
+	),
 )
